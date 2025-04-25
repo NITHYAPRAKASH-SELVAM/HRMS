b460a0206538f67c22d3d70cdc1fff49e9367e4c
NITHYAPRAKASH-SELVAM <selvamnithyaprakash@gmail.com>
2025-04-25 13:54:06 +0530

    u1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 8080;
-console.log(HOST)
-app.listen(HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+const HOST = 'localhost';
+const PORT = 5000;
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
