95a45185c252aebd65ff27aa92f3de4ef12cd005
NITHYAPRAKASH-SELVAM <selvamnithyaprakash@gmail.com>
2025-05-25 16:08:05 +0530

    u35

--- server/src/ml/screen_api.py
+++ server/src/ml/screen_api.py
@@ -2,6 +2,9 @@
 
 import sys
 import json
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from ml.features import extract_features
 from ml.logistic_model import predict_fit
 
